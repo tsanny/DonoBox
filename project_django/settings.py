5011e0ed453fdf63649627805be5a2a9514c228c
unknown <sulthanftr@gmail.com>
2022-10-25 12:29:00 +0700

    Bootstrap, notification: view, model, template

--- project_django/settings.py
+++ project_django/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'example_app',
     'katalog',
+    'notification',
 ]
 
 MIDDLEWARE = [
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
