e1c6b23ab25474a1530f49b0a3e642261a6707d8
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2022-10-28 18:54:48 +0700

    Add ForeignKeys and validation (untested)

--- crowdfund/forms.py
+++ crowdfund/forms.py
@@ -1,12 +1,37 @@
 from .models import Crowdfund, Donation
+from datetime import datetime
 from django.forms import ModelForm
 
 class CrowdfundForm(ModelForm):
     class Meta:
         model = Crowdfund
         fields = ["title", "description", "target", "deadline"]
+    
+    def clean(self):
+        cleaned = super().clean()
+        title = cleaned.get("title")
+        description = cleaned.get("description")
+        target = cleaned.get("target")
+        deadline = cleaned.get("deadline")
+        if len(title) > 50:
+            self.add_error("title", "Judul harus kurang dari 50 karakter.")
+        if len(description) > 1000:
+            self.add_error("description", "Informasi harus kurang dari 1000 karakter.")
+        if target < 1 or target > 1000000000:
+            self.add_error("target", "Target dana harus di antara 1 dan 1000000000.")
+        if deadline < datetime.now():
+            self.add_error("deadline", "Tanggal dan waktu batas harus di masa depan.")
 
 class DonationForm(ModelForm):
     class Meta:
         model = Donation
         fields = ["amount", "comment"]
+
+    def clean(self):
+        cleaned = super().clean()
+        amount = cleaned.get("amount")
+        comment = cleaned.get("comment")
+        if amount < 1 or amount > 1000000000:
+            self.add_error("amount", "Jumlah donasi harus di antara 1 dan 1000000000.")
+        if len(comment) > 1000:
+            self.add_error("comment", "Komentar harus kurang dari 1000 karakter.")
--- crowdfund/models.py
+++ crowdfund/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-# from ..autentikasi.models import Account
+from ..autentikasi.models import Account
 
 class Crowdfund(models.Model):
-    # fundraiser = models.ForeignKey(Account, on_delete = models.CASCADE)
+    fundraiser = models.ForeignKey(Account, on_delete = models.CASCADE)
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=1000)
+    description = models.TextField(blank=True)
     collected = models.PositiveIntegerField()
     target = models.PositiveIntegerField()
     deadline = models.DateTimeField()
 
 class Donation(models.Model):
-    # donator = models.ForeignKey(Account, on_delete = models.CASCADE)
+    donator = models.ForeignKey(Account, on_delete = models.CASCADE)
     crowdfund = models.ForeignKey(Crowdfund, on_delete = models.CASCADE)
     amount = models.PositiveIntegerField()
-    comment = models.CharField(max_length=1000, blank=True)
+    comment = models.TextField(blank=True)
