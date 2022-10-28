69a27ce4037ee701f2e6afb5645e7c2da00c9716
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2022-10-28 18:55:35 +0700

    Add fundraiser views and GET features (untested)

--- crowdfund/urls.py
+++ crowdfund/urls.py
@@ -6,7 +6,10 @@ app_name = "crowdfund"
 urlpatterns = [
     path("", show_crowdfunds, name="show_crowdfunds"),
     path("<int:id>/", show_crowdfund, name="show_crowdfund"),
-    path("json/", show_crowdfunds_json, name="show_crowdfunds_json"),
-    path("json/ongoing/", show_crowdfunds_json_ongoing, name="show_crowdfunds_json_ongoing"),
-    path("json/<int:id>/", show_crowdfund_json, name="show_crowdfund_json"),
+    path("funds/json/ongoing/", show_crowdfunds_json_ongoing, name="show_crowdfunds_json_ongoing"),
+    path("funds/json/myfunds/", show_crowdfunds_json_by_me, name="show_crowdfund_json_by_me"),
+    path("funds/json/<int:id>/", show_crowdfund_json, name="show_crowdfund_json"),
+    path("donations/json/<int:id>/", show_donations_json_by_fund, name="show_donations_json_by_fund"),
+    path("funds/add/", add_crowdfund, name="add_crowdfund"),
+    path("donations/add/", add_donation, name="add_donation")
 ]
