fed4e6ff5b06570bea7f1d27a0b3b1ed33babbf2
unknown <sulthanftr@gmail.com>
2022-11-02 22:44:38 +0700

    Add FrequentlyAskedQuestion model jawaban field

--- homepage/models.py
+++ homepage/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class FrequentlyAskedQuestion(models.Model):
     user = models.CharField(max_length=40,blank=True, null=True)
     pertanyaan = models.TextField()
+    jawaban = models.TextField(null=True, blank=True, default="Belum ada jawaban")
